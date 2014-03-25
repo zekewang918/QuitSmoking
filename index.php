2752e6538e5c5894a2d043d8b9b76a71a8253efc
amr-zokari <amr.zokari@gmail.com>
2014-03-25 18:16:55 -0300

    CSS style
    the css styling is added for the picture

--- index.php
+++ index.php
@@ -115,6 +115,20 @@
         <link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.css">
         <script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
         <script src="http://code.jquery.com/mobile/1.3.2/jquery.mobile-1.3.2.min.js"></script>
+        
+        <!-- CSS styling -->
+        <style>
+        
+        
+        .smoke {
+       
+         height = "30%";
+         width = "30%";
+         margin-left = "30%";
+        }
+        
+        </style>
+
     </head>
     
     <body>
