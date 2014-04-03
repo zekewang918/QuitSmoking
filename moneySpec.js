a66edee1b6a4f78361c0b81d87ccac46c3502fa0
GianlucaRi <gianluca.ricciardelli.pro@gmail.com>
2014-04-03 18:04:46 -0300

    First update of the spec / Not finished

--- moneySpec.js
+++ moneySpec.js
@@ -1,40 +1,60 @@
 // Specfile of money.js
 // all jasmine test are here
 
-describe("test the funtion totalMoneySpent ", function() {
+describe("test the function getPricePerCigarette ", function() {
+	var spyEven;
+
+	it("Test good and bad cases", function() {
+	   var spyEven = spyOnEvent( ,'click'); // pb here
+	   //Good cases :
+	   var a = getPricePerCigarette();
+
+	  // division by zero
+	  var b = isNaN(getPricePerCigarette());
+	  // negative
+	  var c = null;
+	  
+	  expect(a).toEqual(1);
+	  expect(b).toBe(true);
+      expect(c).toEqual(-1);
+    });
+});		
+	
+
+
+describe("test the function totalMoneySpent ", function() {
 
     it("Test normal case", function() {
-      var a = totalMoneySpent(100, 10, 20);
-	  var b = totalMoneySpent(0, 10, 20);
+      var a = totalMoneySpent(100, 0.652);
+	  var b = totalMoneySpent(0, 0.50);
 	  
-      expect(a).toEqual(50);
+      expect(a).toEqual(65);
 	  expect(b).toEqual(0);
     });
 	
-	
-	it("Test normal bad cases", function() {
-	  // division by zero
-	  var a = isNaN(totalMoneySpent(100, 10, 0));
-	  
-	  // negative
-	  var b = totalMoneySpent(100, 10, -20);
-	  var c = totalMoneySpent(100, -10, 20);
-	  
-	  expect(a).toBe(true);
-      expect(b).toEqual(-50);
-	  expect(c).toEqual(-50);
-    });
-	
-	
-});
+});	
 
-describe("test the funtion getValues ", function() {
+describe("test the function totalMoneySaved ", function() {
+
+    it("Test normal case", function() {
+      var a = totalMoneySaved(100, 150, 0.5);
+	  // Test if the money saved is zero when the previous consumption is greater than the actual
+	  var b = totalMoneySaved(200, 100, 1.2);
+	  
+      expect(a).toEqual(25);
+	  expect(b).toEqual(0);
+    });
+});	
+
+
+describe("test the function moneyStatus ", function() {
 
     it("Test normal case", function() {
 	
 	// Simulate user inputs :
 	
-      var a = getValues(100); // will not pass
+      var a = moneyStatus(100, 200); 
+	  // will not pass does not return anything and depend on the input
 	  expect(a).toEqual(50);
     });
 	
